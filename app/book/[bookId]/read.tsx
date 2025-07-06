df0d1122e4c02b31a40066bd193553270e3f4ae5
Matthew Christensen <christensen.developer@gmail.com>
2025-07-05 22:40:10 -0600

    finish header, finally get remove books loading by downloading

--- app/book/[bookId]/read.tsx
+++ app/book/[bookId]/read.tsx
@@ -3,9 +3,13 @@ import { useFileSystem } from "@epubjs-react-native/expo-file-system";
 import { BottomSheetFlashList, BottomSheetModal, BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { BottomSheetModalMethods } from "@gorhom/bottom-sheet/lib/typescript/types";
 import { HeaderBackButton } from "@react-navigation/elements";
+import { useTheme } from "@react-navigation/native";
+import * as FileSystem from "expo-file-system";
+import { DownloadProgressData } from "expo-file-system";
+import { useLocalSearchParams, useNavigation } from "expo-router";
 import { getItem } from "expo-secure-store";
 import { cssInterop } from "nativewind";
-import React, { forwardRef, useCallback, useMemo, useRef, useState } from "react";
+import React, { forwardRef, useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { useWindowDimensions, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
@@ -18,14 +22,19 @@ import { VoidCallback } from "~/lib/types";
 const BottomModal = cssInterop(BottomSheetModal, { className: 'style', handleClassName: 'handleStyle', bgClassName: 'backgroundStyle' });
 
 export default function ReadBook() {
-  const server = getItem(SecureStorageKeys.server);
+
   const { width, height } = useWindowDimensions();
   const insets = useSafeAreaInsets();
+  const { colors } = useTheme();
+
+  const { goBack } = useNavigation();
+
+  const server = getItem(SecureStorageKeys.server);
 
   const tocRef = useRef<BottomSheetModal>(null);
   const bookmarksRef = useRef<BottomSheetModal>(null);
 
-  // const { bookId } = useLocalSearchParams<{ bookId: string }>();
+  const { bookId } = useLocalSearchParams<{ bookId: string }>();
 
   const { bookmarks, isBookmarked, addBookmark, removeBookmark, getCurrentLocation } = useReader();
 
@@ -48,10 +57,6 @@ export default function ReadBook() {
     }
   }, [getCurrentLocation, addBookmark, removeBookmark, isBookmarked, bookmarks]);
 
-  // const goToSection = useCallback((section: Section) => {
-  //   goToLocation(section.href.split('/')[1]);
-  // }, [goToLocation]);
-
   const closeToC = useCallback(() => {
     tocRef?.current?.dismiss();
   }, [tocRef]);
@@ -61,37 +66,57 @@ export default function ReadBook() {
   }, [bookmarksRef]);
 
   const [isFullScreen, setIsFullScreen] = useState(false);
+
+  const [downloadProgress, setDownloadProgress] = useState<number>(0);
+
+  const onDownloadProgress = (downloadProgress: DownloadProgressData) => {
+    const progress = downloadProgress.totalBytesWritten / downloadProgress.totalBytesExpectedToWrite;
+    setDownloadProgress(progress);
+  };
+
+  const download = FileSystem.createDownloadResumable(
+    `${server}/api/v1/books/${bookId}/file`,
+    FileSystem.documentDirectory + `${bookId}.epub`,
+    {},
+    onDownloadProgress,
+  );
+
+  useEffect(() => {
+    download.downloadAsync();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <GestureHandlerRootView
       style={{
         flex: 1,
-        paddingTop: insets.top,
+        paddingTop: isFullScreen ? insets.top : 0,
         paddingBottom: insets.bottom,
         paddingLeft: insets.left,
         paddingRight: insets.right,
+        backgroundColor: colors.card,
       }}
     >
       <View 
-        className={`${isFullScreen ? 'hidden' : ''} h-16 bg-gray-600 flex flex-row items-center`}
+        className={`${isFullScreen ? 'hidden' : ''} flex flex-row items-center`}
+        style={{ paddingTop: insets.top, backgroundColor: colors.card, }}
       >
-        <HeaderBackButton tintColor="#ffffff"/>
+        <HeaderBackButton tintColor={colors.text} onPress={goBack} />
         <View className="flex-1 flex flex-row justify-end">
           <Button
             variant="ghost"
             onPress={toggleBookmark}
             onLongPress={() => bookmarksRef.current?.present()}
           >
-            <BookmarkIcon className={`stroke-white ${isBookmarked ? 'fill-white' : ''}`} />
+            <BookmarkIcon stroke={colors.text} fill={isBookmarked ? colors.text : ''} />
           </Button>
           <Button variant="ghost" onPress={() => tocRef.current?.present()}>
-            <Chapters className="stroke-white" />
+            <Chapters stroke={colors.text} />
           </Button>
         </View>
       </View>
-      <Reader
-        src="https://s3.amazonaws.com/moby-dick/OPS/package.opf"
-        // src="https://komga.empko.net/api/v1/books/0KTBR74SSVKNV/file.epub"
-        // src={`${server}/api/v1/books/${bookId}/manifest/epub`}
+      {downloadProgress >= 1 && <Reader
+        src={FileSystem.documentDirectory + `${bookId}.epub`}
         fileSystem={useFileSystem}
         width={width}
         height={isFullScreen ? height : height-insets.top}
@@ -100,7 +125,7 @@ export default function ReadBook() {
           console.log("double tapped");
           setIsFullScreen((prev) => !prev);
         }}
-      />
+      />}
 
       <TableOfContents ref={tocRef} close={closeToC} />
       <BookmarksList ref={bookmarksRef} close={closeBookmarks} />
