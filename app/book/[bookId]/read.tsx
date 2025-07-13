178305286600cdfab417145515f1b4a4ca0f7ec2
Matthew Christensen <christensen.developer@gmail.com>
2025-07-12 21:46:38 -0600

    reader settings menu

--- app/book/[bookId]/read.tsx
+++ app/book/[bookId]/read.tsx
@@ -1,4 +1,4 @@
-import { Bookmark, Reader, Section, useReader } from "@epubjs-react-native/core";
+import { Bookmark, Reader, Section, Themes, useReader } from "@epubjs-react-native/core";
 import { useFileSystem } from "@epubjs-react-native/expo-file-system";
 import { BottomSheetFlashList, BottomSheetModal, BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { BottomSheetModalMethods } from "@gorhom/bottom-sheet/lib/typescript/types";
@@ -13,10 +13,11 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Button } from "~/components/ui/button";
 import { Text } from "~/components/ui/text";
-import { BookmarkIcon, Chapters, CloseIcon } from "~/lib/icons";
+import { ToggleGroup, ToggleGroupItem } from "~/components/ui/toggle-group";
+import { BookmarkIcon, Chapters, CloseIcon, Settings } from "~/lib/icons";
 import { VoidCallback } from "~/lib/types";
 
-const BottomModal = cssInterop(BottomSheetModal, { className: 'style', handleClassName: 'handleStyle', bgClassName: 'backgroundStyle' });
+const BottomModal = cssInterop(BottomSheetModal, { className: 'style', handleClassName: 'handleIndicatorStyle', bgClassName: 'backgroundStyle' });
 
 export default function ReadBook() {
 
@@ -28,6 +29,7 @@ export default function ReadBook() {
 
   const tocRef = useRef<BottomSheetModal>(null);
   const bookmarksRef = useRef<BottomSheetModal>(null);
+  const settingsRef = useRef<BottomSheetModal>(null);
 
   const { bookId } = useLocalSearchParams<{ bookId: string }>();
 
@@ -53,12 +55,16 @@ export default function ReadBook() {
   }, [getCurrentLocation, addBookmark, removeBookmark, isBookmarked, bookmarks]);
 
   const closeToC = useCallback(() => {
-    tocRef?.current?.dismiss();
+    tocRef.current?.dismiss();
   }, [tocRef]);
 
   const closeBookmarks = useCallback(() => {
-    bookmarksRef?.current?.dismiss();
+    bookmarksRef.current?.dismiss();
   }, [bookmarksRef]);
+
+  const closeSettings = useCallback(() => {
+    settingsRef.current?.dismiss();
+  }, [settingsRef]);
 
   const [isFullScreen, setIsFullScreen] = useState(false);
 
@@ -84,10 +90,13 @@ export default function ReadBook() {
             onPress={toggleBookmark}
             onLongPress={() => bookmarksRef.current?.present()}
           >
-            <BookmarkIcon stroke={colors.text} fill={isBookmarked ? colors.text : ''} />
+            <BookmarkIcon stroke={colors.text} fill={isBookmarked ? colors.text : 'transparent'} />
           </Button>
           <Button variant="ghost" onPress={() => tocRef.current?.present()}>
             <Chapters stroke={colors.text} />
+          </Button>
+          <Button variant="ghost" onPress={() => settingsRef.current?.present()}>
+            <Settings stroke={colors.text} />
           </Button>
         </View>
       </View>
@@ -98,13 +107,13 @@ export default function ReadBook() {
         height={isFullScreen ? height : height-insets.top}
         waitForLocationsReady
         onDoubleTap={() => {
-          console.log("double tapped");
           setIsFullScreen((prev) => !prev);
         }}
       />
 
       <TableOfContents ref={tocRef} close={closeToC} />
       <BookmarksList ref={bookmarksRef} close={closeBookmarks} />
+      <SettingsSheet ref={settingsRef} close={closeSettings} />
     </GestureHandlerRootView>
   );
 }
@@ -144,6 +153,7 @@ const TableOfContents = forwardRef<
         enablePanDownToClose
         className="w-full flex-1 px-5 mt-4 shadow shadow-neutral-800"
         bgClassName="dark:bg-neutral-700"
+        handleClassName="dark:bg-neutral-300"
       >
         <View className="flex flex-row items-center justify-between">
           <Text className="text-2xl font-semibold">
@@ -222,6 +232,7 @@ const BookmarksList = forwardRef<
         enablePanDownToClose
         className="w-full flex-1 px-5 mt-4 shadow shadow-neutral-800"
         bgClassName="dark:bg-neutral-700"
+        handleClassName="dark:bg-neutral-300"
       >
         <View className="flex flex-row items-center justify-between">
           <Text className="text-2xl font-semibold">
@@ -239,6 +250,121 @@ const BookmarksList = forwardRef<
           renderItem={renderItem}
           style={{ width: '100%' }}
         />
+      </BottomModal>
+    </BottomSheetModalProvider>
+  );
+});
+
+const MAX_FONT_SIZE = 32;
+const MIN_FONT_SIZE = 10;
+
+// eslint-disable-next-line react/display-name
+const SettingsSheet = forwardRef<
+  BottomSheetModalMethods,
+  { close: VoidCallback; }
+>(({ close }, ref) => {
+  const {
+    changeTheme,
+    changeFontSize,
+    changeFontFamily,
+    changeFlow,
+  } = useReader();
+
+  const [selectedTheme, setSelectedTheme] = useState<keyof typeof Themes>('LIGHT');
+  const [font, setFont] = useState<string>("Helvetica");
+  const [fontSize, setFontSize] = useState(16);
+  const [ flow, setFlow ] = useState<'paginated' | 'scrolled'>('paginated');
+
+  const updateTheme = useCallback((newTheme: string | undefined) => {
+    if (!newTheme) return;
+    setSelectedTheme(newTheme as keyof typeof Themes);
+    changeTheme(Themes[newTheme as keyof typeof Themes]);
+  }, [changeTheme]);
+
+  const updateFont = useCallback((newFont: string | undefined) => {
+    if (!newFont) return;
+    setFont(newFont);
+    changeFontFamily(newFont);
+  }, [changeFontFamily]);
+
+  const increaseFontSize = () => {
+    if (fontSize < MAX_FONT_SIZE) {
+      setFontSize(fontSize + 1);
+      changeFontSize(`${fontSize + 1}px`);
+    }
+  };
+
+  const decreaseFontSize = () => {
+    if (fontSize > MIN_FONT_SIZE) {
+      setFontSize(fontSize - 1);
+      changeFontSize(`${fontSize - 1}px`);
+    }
+  };
+
+  const updateFlow = useCallback((newFlow: string | undefined) => {
+    if (!newFlow) return;
+    setFlow(newFlow as 'paginated' | 'scrolled');
+    changeFlow(newFlow as 'paginated' | 'scrolled');
+  }, [changeFlow]);
+
+  return (
+    <BottomSheetModalProvider>
+      <BottomModal
+        ref={ref}
+        index={0}
+        snapPoints={['35%']}
+        enableDynamicSizing={false}
+        enablePanDownToClose
+        className="w-full flex-1 px-5 mt-4 shadow shadow-neutral-800"
+        bgClassName="dark:bg-neutral-700"
+        handleClassName="dark:bg-neutral-300"
+      >
+        <View className="flex flex-row justify-end">
+          <Button variant="ghost" onPress={close}>
+            <CloseIcon className="dark:stroke-white" />
+          </Button>
+        </View>
+        <ToggleGroup type="single" onValueChange={updateTheme} value={selectedTheme} className="justify-start">
+          <ToggleGroupItem value="LIGHT" size="lg">
+            <View className="w-8 h-8 bg-white border border-black dark:border-none rounded-full"></View>
+          </ToggleGroupItem>
+          <ToggleGroupItem value="DARK" size="lg">
+            <View className="w-8 h-8 bg-black dark:border dark:border-white rounded-full"></View>
+          </ToggleGroupItem>
+          <ToggleGroupItem value="SEPIA" size="lg">
+            <View className="w-8 h-8 bg-amber-200 dark:border dark:border-white rounded-full"></View>
+          </ToggleGroupItem>
+        </ToggleGroup>
+        <View className="flex flex-row items-center gap-x-2 mt-4">
+          <ToggleGroup type="single" onValueChange={updateFont} value={font}>
+            <ToggleGroupItem value="Helvetica" size="lg">
+              <Text className="font-[Helvetica]">Aa</Text>
+            </ToggleGroupItem>
+            <ToggleGroupItem value="Georgia" size="lg">
+              <Text className="font-[Georgia]">Aa</Text>
+            </ToggleGroupItem>
+            <ToggleGroupItem value="Courier New" size="lg">
+              <Text className="font-[Courier]">Aa</Text>
+            </ToggleGroupItem>
+          </ToggleGroup>
+          <View className="flex-1 flex flex-row items-center justify-evenly">
+            <Button variant="secondary" onPress={decreaseFontSize} disabled={fontSize <= MIN_FONT_SIZE} size="icon">
+              <Text className="text-2xl">-</Text>
+            </Button>
+            <Text className="w-12 text-lg text-center">{fontSize}</Text>
+            <Button variant="secondary" onPress={increaseFontSize} disabled={fontSize >= MAX_FONT_SIZE} size="icon">
+              <Text className="text-2xl">+</Text>
+            </Button>
+          </View>
+        </View>
+        <ToggleGroup type="single" onValueChange={updateFlow} value={flow} className="justify-center mt-4">
+          <ToggleGroupItem value="paginated" size="lg" className="flex-1">
+            <Text>Paginated</Text>
+          </ToggleGroupItem>
+          <ToggleGroupItem value="scrolled" size="lg" className="flex-1">
+            <Text>Scrolled</Text>
+          </ToggleGroupItem>
+        </ToggleGroup>
       </BottomModal>
     </BottomSheetModalProvider>
   );
--- lib/query/bookDetails.ts
+++ lib/query/bookDetails.ts
@@ -6,6 +6,8 @@ import { tryCatch } from "../utils";
 export const useBookDetails = (bookId: string) => useQuery({
     queryKey: ['book-details', bookId],
     queryFn: async () => {
+        // TODO try to fetch from local storage first
+        // if not found, fetch from server
         const [response, error] = await tryCatch(
             axios.get(`/api/v1/books/${bookId}`)
         );
